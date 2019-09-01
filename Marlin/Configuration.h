5f4c3ac5b00778489b3e73ba8cb02bc7e6a86396
Mark <pal.mark28@gmail.com>
2019-09-01 16:13:34 +0200

    max accel reduced

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -734,7 +734,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2000, 1500, 200, 7000 }
+#define DEFAULT_MAX_ACCELERATION      { 1500, 1000, 200, 7000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
